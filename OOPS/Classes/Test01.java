e91341b5cab904929bd052a1f24fba48ed71b338
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2023-10-19 16:47:53 +0530

    Oops Classes

---
+++ OOPS/Classes/Test01.java
@@ -0,0 +1,25 @@
+package OOPS.Classes;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		
+		ClassEx01 c1 = new ClassEx01();
+		
+		ClassEx01 c2 = new ClassEx01();
+		
+		//dot(.) saperater
+		c1.a=100;
+		System.out.println(c1.a);
+		
+		c2.a=200;
+		System.out.println(c2.a);
+		
+		
+		
+		c1.m1();
+		
+
+	}
+
+}
