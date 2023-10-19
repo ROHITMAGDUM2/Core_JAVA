dea7c4df3dfba5557c41bdeacc398d5e1426a7da
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2023-10-19 16:51:32 +0530

    Oops2

--- OOPS/Classes/Test01.java
+++ OOPS/Classes/Test01.java
@@ -3,22 +3,19 @@ package OOPS.Classes;
 public class Test01 {
 
 	public static void main(String[] args) {
-		
+
 		ClassEx01 c1 = new ClassEx01();
-		
+
 		ClassEx01 c2 = new ClassEx01();
-		
-		//dot(.) saperater
-		c1.a=100;
+
+		// dot(.) saperater
+		c1.a = 100;
 		System.out.println(c1.a);
-		
-		c2.a=200;
+
+		c2.a = 200;
 		System.out.println(c2.a);
 		
-		
-		
 		c1.m1();
-		
 
 	}
 
