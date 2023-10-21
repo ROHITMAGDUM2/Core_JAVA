044787087699ed84da57d9ccc35c78383addc6f8
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2023-10-21 19:30:22 +0530

    threads

--- multithreading/threads/ThreadNameMethods.java
+++ multithreading/threads/ThreadNameMethods.java
@@ -1,6 +1,6 @@
 package multithreading.threads;
 
-class UserThread extends Thread{
+class UserThread extends Thread {
 	public void run() {
 		System.out.println(Thread.currentThread().getName());
 		System.out.println("User Deffinered Thread 01 " + Thread.currentThread().getName());
@@ -8,21 +8,21 @@ class UserThread extends Thread{
 }
 
 public class ThreadNameMethods {
-	
+
 	public static void main(String[] args) {
-		
-		System.out.println("Hello "+Thread.currentThread().getName());
+
+		System.out.println("Hello " + Thread.currentThread().getName());
 		System.out.println(Thread.currentThread().getName());
 		Thread.currentThread().setName("Rohit");
-		System.out.println("New Thread Name= "+Thread.currentThread().getName());
-		
-	 UserThread ut =new UserThread();
-	 ut.start();
-	 
-	 UserThread ut2 =new UserThread();
-	 ut2.start();
-	 System.out.println(Thread.currentThread().isAlive());
-	 System.out.println(ut.isAlive());
+		System.out.println("New Thread Name= " + Thread.currentThread().getName());
+
+		UserThread ut = new UserThread();
+		ut.start();
+
+		UserThread ut2 = new UserThread();
+		ut2.start();
+		System.out.println(Thread.currentThread().isAlive());
+		System.out.println(ut.isAlive());
 	}
 
 }
