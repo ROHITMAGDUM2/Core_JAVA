ab8545b0a846bfcc526ffe993b5e08894b49d6ab
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2023-10-21 19:27:50 +0530

    cases

--- multithreading/cases/MultipleTaskFromMultipleThread.java
+++ multithreading/cases/MultipleTaskFromMultipleThread.java
@@ -1,28 +1,27 @@
 package multithreading.cases;
 
-
-class MyThread03 extends Thread{
+class MyThread03 extends Thread {
 //	task1
 	public void run() {
 		System.out.println("Task 01");
 	}
 }
 
-class Thread2 extends Thread{
+class Thread2 extends Thread {
 //	task2
 	public void run() {
 		System.out.println("Task 02");
 	}
 }
 
-class Thread3 extends Thread{
+class Thread3 extends Thread {
 //	task3
 	public void run() {
 		System.out.println("Task 03");
 	}
 }
 
-class Thread4 extends Thread{
+class Thread4 extends Thread {
 //	task4
 	public void run() {
 		System.out.println("Task 04");
@@ -33,13 +32,13 @@ public class MultipleTaskFromMultipleThread {
 	public static void main(String[] args) {
 		MyThread03 th1 = new MyThread03();
 		th1.start();
-		
+
 		Thread2 th2 = new Thread2();
 		th2.start();
-		
+
 		Thread3 th3 = new Thread3();
 		th3.start();
-		
+
 		Thread4 th4 = new Thread4();
 		th4.start();
 	}
