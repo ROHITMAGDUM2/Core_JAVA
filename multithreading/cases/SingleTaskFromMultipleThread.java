4fb94576c22f5cb489309296544c0f8464b4cdda
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2023-10-21 19:25:40 +0530

    cases

--- multithreading/cases/SingleTaskFromMultipleThread.java
+++ multithreading/cases/SingleTaskFromMultipleThread.java
@@ -2,22 +2,21 @@ package multithreading.cases;
 
 //performing single task from multiple thread
 
-class MyThread extends Thread{
+class MyThread extends Thread {
 //	task
 	public void run() {
 		System.out.println("Single Task 01");
 	}
 }
 
-
 public class SingleTaskFromMultipleThread {
 // main is a thread by JVM
 	public static void main(String[] args) {
-		
+
 //		1st thread
 		MyThread th1 = new MyThread();
 		th1.start();
-		
+
 //		2nd thread
 		MyThread th2 = new MyThread();
 		th2.start();
