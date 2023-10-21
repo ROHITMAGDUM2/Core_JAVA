4e0313a779ccda84d75c8fa9094eab99e60470bf
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2023-10-21 19:23:15 +0530

    cwr

--- multithreading/cwrthreads/RunThreads.java
+++ multithreading/cwrthreads/RunThreads.java
@@ -2,11 +2,11 @@ package multithreding.cwrthreads;
 
 public class RunThreads {
 	public static void main(String[] args) {
-		MyThread1 mt1= new MyThread1();
-		
+		MyThread1 mt1 = new MyThread1();
+
 		mt1.start();
-		
-		mt1.run();// its a normal method not a Thread 
-		
+
+		mt1.run();// its a normal method not a Thread
+
 	}
 }
