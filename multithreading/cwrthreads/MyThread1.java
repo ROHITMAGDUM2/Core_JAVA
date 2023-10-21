51369f38040a8c7523d8f5beb9a7423bc6aabaad
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2023-10-21 19:21:39 +0530

    cwr

--- multithreading/cwrthreads/MyThread1.java
+++ multithreading/cwrthreads/MyThread1.java
@@ -1,10 +1,10 @@
 package multithreding.cwrthreads;
 
-public class MyThread1 extends Thread{
-	
+public class MyThread1 extends Thread {
+
 	@Override
 	public void run() {
 		System.out.println("I am a thread and i am  running");
 	}
-	
+
 }
