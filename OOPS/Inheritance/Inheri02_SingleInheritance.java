f7ce745a4de5c9d0a58129f14b03a4b5212b4017
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2023-10-22 23:50:29 +0530

    inheritance

--- OOPS/Inheritance/Inheri02_SingleInheritance.java
+++ OOPS/Inheritance/Inheri02_SingleInheritance.java
@@ -1,12 +1,12 @@
 package OOPS.Inheritance;
 
-class AClass{
+class AClass {
 	void showA() {
 		System.out.println("A class Method");
 	}
 }
 
-class BClass extends AClass{
+class BClass extends AClass {
 	void showB() {
 		System.out.println("B class Method");
 	}
@@ -16,16 +16,16 @@ public class Inheri02_SingleInheritance {
 
 	public static void main(String[] args) {
 		System.out.println("Single Inheritance");
-		
+
 		AClass obj1 = new AClass();
 		obj1.showA();
 //		obj2.showB();
-		
+
 		System.out.println("--------------");
 		BClass obj2 = new BClass();
 		obj2.showA();
 		obj2.showB();
-		
+
 	}
 
 }
