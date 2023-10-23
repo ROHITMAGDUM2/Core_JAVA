27c8741f98b355594fbf9d8fc2a82e8d93c038bf
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2023-10-23 22:49:25 +0530

    Fix issue of assignments #2
    Co-authored-by: Rohit Magddum <rohitmagdum2@gmail.com>

---
+++ assignments/_02CommentsTypes.java
@@ -0,0 +1,20 @@
+package assignments;
+
+public class _02CommentsTypes {
+	public static void main(String[] args) {
+		//types of Comments
+		//1. single line comment====>	//
+		//2. Multiple line Comments=====> /*      */  
+		//3. Document Comment ====/**      */
+		
+		
+		/**
+		//values for addition
+		int a= 10;   //first value
+		int b = 20;   // second value 
+		*/
+		
+		// main statemain
+		//monst imp statet
+	}
+}
