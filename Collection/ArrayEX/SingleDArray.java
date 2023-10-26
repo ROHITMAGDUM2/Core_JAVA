7a108180fc599b148a536712eeacd603a44dd563
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2023-10-26 19:45:43 +0530

    array Program #02 Co-authored-by: Rohit Magddum <rohitmagdum2@gmail.com>

---
+++ Collection/ArrayEX/SingleDArray.java
@@ -0,0 +1,15 @@
+package ArrayEX;
+
+public class SingleDArray {
+
+	public static void main(String[] args) {
+		
+		int[] Ar= new int[3];
+		Ar[0]= 11;
+		Ar[1]= 12;
+		Ar[2]= 13;
+		
+		System.out.println(Ar[2]);
+	}
+
+}
