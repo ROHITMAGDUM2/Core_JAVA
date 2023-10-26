09e2e90df45001c5ea9a1973f7cb7fa08c8be839
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2023-10-26 19:50:36 +0530

    collection Program #10 Co-authored-by: Rohit Magdum <rohitmagdum2@gmail.com>

---
+++ Collection/collectionCodes/LinkListEx1.java
@@ -0,0 +1,28 @@
+package collectionCodes;
+
+import java.util.LinkedList;
+
+public class LinkListEx1 {
+
+	public static void main(String[] args) {
+			
+		LinkedList<Integer> al= new LinkedList<Integer>();
+			
+			System.out.println(al.size());
+			
+			al.add(111);
+			al.add(234);
+			al.add(123);
+			
+			
+			System.out.println(al.size());
+			
+			System.out.println();
+			
+			al.add(1,222);
+		
+			
+			
+	}
+
+}
