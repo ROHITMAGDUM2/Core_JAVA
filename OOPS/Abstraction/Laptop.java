44acb97d71a5006e4e421e6670329efc54dca7d9
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2023-11-02 19:12:53 +0530

    Abstaction Program #04 Co-authored-by: Rohit Magddum <rohitmagdum2@gmail.com>

---
+++ OOPS/Abstraction/Laptop.java
@@ -0,0 +1,19 @@
+package OOPS.Abstraction;
+//100 % Abstraction
+//syntactically similar to classes,
+public interface Laptop {
+	//filds
+	public static final int a = 0;
+	//abstract method// proprty
+	void software();
+	
+	default void charger() {
+		
+	}
+	
+	static void tuchPad() {
+		
+	}
+}
+
+
