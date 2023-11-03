80298ff587347954aad3aca7aed428a84ba79c06
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2023-11-03 19:30:16 +0530

    super keyword Program #08 Co-authored-by: Rohit Magddum <rohitmagdum2@gmail.com>

---
+++ OOPS/keywords/finalkeyword/_03_FinalClass.java
@@ -0,0 +1,27 @@
+package OOPS.keywords.finalkeyword;
+/**
+ * 
+ * @author RM
+ * 
+ * final class:
+ *  If we crete any final class we can not extend it or inherit it
+ *
+ */
+final class Student{
+	
+}
+//final class we can not extend
+/*
+ * class Coder extends Student{ //The type Coder cannot subclass the final class
+ * Student
+ * 
+ * }
+ */
+
+public class _03_FinalClass {
+
+	public static void main(String[] args) {
+//		Coder co = new Coder();
+	}
+
+}
