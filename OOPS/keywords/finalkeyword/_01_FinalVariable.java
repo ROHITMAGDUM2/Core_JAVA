0fe266c35de84591c6c837f16209c197d6ec9269
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2023-11-03 19:29:07 +0530

    super keyword Program #06 Co-authored-by: Rohit Magddum <rohitmagdum2@gmail.com>

---
+++ OOPS/keywords/finalkeyword/_01_FinalVariable.java
@@ -0,0 +1,23 @@
+package OOPS.keywords.finalkeyword;
+
+/**
+ * 
+ * @author RM
+ * 
+ * Final Variable:
+ *  if we create any final variable it become constant.
+ *  we can not change the value of final variable
+ *
+ */
+public class _01_FinalVariable {
+
+	public static void main(String[] args) {
+		final int a=10;
+		
+//		a = a + 20; // The final local variable a cannot be assigned. 
+//		It must be blank and not using a compound assignment
+		
+		System.out.println(a);
+	}
+
+}
