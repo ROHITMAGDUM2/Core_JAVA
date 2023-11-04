f08ea31c9fc1aa24fc03aee313a762017551b80f
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2023-11-04 19:03:51 +0530

    array Program #03 Co-authored-by: Rohit Magddum <rohitmagdum2@gmail.com>

---
+++ assignments/ThisDemo.java
@@ -0,0 +1,13 @@
+
+public class ThisDemo {
+	static int num=10;
+	public static void java() {
+		System.out.println("Java"+num);
+		}
+	public static void main(String[] args) {
+		
+		ThisDemo.java();
+		
+	}
+}
+
