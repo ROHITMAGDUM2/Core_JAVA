2c9a210b54a6d687e990ac9c944c96a12ee18e5c
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2023-11-04 19:04:33 +0530

    array Program #04 Co-authored-by: Rohit Magddum <rohitmagdum2@gmail.com>

---
+++ assignments/game1/Ekno.java
@@ -0,0 +1,19 @@
+package game1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ekno {
+	public static void main(String[] arg) {
+		
+		
+		
+		
+		
+
+		List<Integer> l = new ArrayList<>();
+
+		l.add(10);
+
+	}
+}
