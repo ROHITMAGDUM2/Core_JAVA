c8556ee857900f55b9243f5790104cfa8f028167
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2023-11-04 19:07:43 +0530

    assignment Program #09 Co-authored-by: Rohit Magddum <rohitmagdum2@gmail.com>

--- assignments/ArrayDemo1.java
+++ assignments/ArrayDemo1.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 
 public class ArrayDemo1 {
-public static void main(String[] args) {
-	int a;
-	Scanner sc=new Scanner(System.in);
-	System.out.println("Enter Length of array= ");
-	a=sc.nextInt();
-	int b= sc.nextInt();
-	
-	int[][]arr=new int[a][b];
-	for(int i=0; i<=a;i++ ) {
-		System.out.println(i);
-		for (int j=0 ; j<=b;j++) {
-			System.out.println(j);
+	public static void main(String[] args) {
+		int a;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Length of array= ");
+		a = sc.nextInt();
+		int b = sc.nextInt();
+
+		int[][] arr = new int[a][b];
+		for (int i = 0; i <= a; i++) {
+			System.out.println(i);
+			for (int j = 0; j <= b; j++) {
+				System.out.println(j);
+			}
 		}
 	}
-}
 }
