21836c9d1d9fd4661e13aaac5fbbc620f27f073f
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2023-11-04 19:03:16 +0530

    array Program #02 Co-authored-by: Rohit Magddum <rohitmagdum2@gmail.com>

---
+++ assignments/test/TestA.java
@@ -0,0 +1,10 @@
+//
+//package test;
+// 
+// public class TestA {
+//
+//public static void main(String[] args) { 
+//	p1.A a1 = new p1.A(); a1.show();
+//	p2.A a2 = new p2.A(); a2.display(); 
+//	}
+//}
